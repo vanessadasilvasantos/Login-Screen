4ae50912ef17691fcfc8ff9d57f00cdad86a1e0f
vanessadasilvasantos <vanessadasilvasantos65@gmail.com>
2023-06-06 14:50:37 -0300

    missao 2 concluida

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./img/logo.png")} />
+      <Text style={styles.titulo}>Acesse sua conta</Text>
+      <View style={styles.input}>
+        <Image source={require("./img/email.png")} />
+        <TextInput
+          placeholder="Digite seu e-mail"
+          placeholderTextColor={"#7C7C8A"}
+          style={styles.inputText}
+        />
+      </View>
+      <View style={styles.input}>
+        <Image source={require("./img/key.png")} />
+        <TextInput
+          placeholder="Digite sua senha"
+          placeholderTextColor={"#7C7C8A"}
+          style={styles.inputText}
+          secureTextEntry={true}
+        />
+      </View>
+      <TouchableOpacity style={styles.buttonContainer}>
+        <Text style={styles.button}>Entrar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +40,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#202024",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    color: "#7C7C8A",
+    flexDirection: "row",
+    backgroundColor: "#121212",
+    gap: 5,
+    width: "80%",
+    alignItems: "center",
+    padding: 10,
+    borderRadius: 4,
+    marginBottom: 10,
+  },
+  titulo: {
+    fontSize: 18,
+    marginBottom: 20,
+    color: "#E1E1E6",
+  },
+  buttonContainer: {
+    width: "80%",
+    marginTop: 10,
+    padding: 15,
+    borderRadius: 4,
+    backgroundColor: "#E51C44",
+  },
+  button: {
+    textAlign: "center",
+    fontWeight: 600,
+    color: "#fff",
+  },
+  inputText: {
+    color: "#7C7C8A",
   },
 });
